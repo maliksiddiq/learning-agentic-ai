a1910d92d0ffd8f03b5b458a142fb4c1641fbd22
maliksiddiq <maliksiddiqawan21@gmail.com>
2026-08-22 22:09:06 -0900

    Python Journey!

---
+++ 01_ Python_For_ Agentic_ Ai/01 Data types/Data types.py
@@ -0,0 +1,12 @@
+print('Start Learning Python')
+
+print('Data types in Python')
+
+print('1. Numbers')
+print('2. Strings')
+print('3. Lists')
+print('4. Tuples')
+print('5. Sets')
+print('6. Dictionaries')
+print('7. Boolean')
+print('8. None')
